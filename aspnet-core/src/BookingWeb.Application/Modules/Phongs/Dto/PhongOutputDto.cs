f8755de6aa87cf26d20f85c3924a530ef7b9d393
Tien192 <tranxuantien192@gmail.com>
2023-07-04 17:37:52 +0700

    04/07 - mai lafm tieps

---
+++ aspnet-core/src/BookingWeb.Application/Modules/Phongs/Dto/PhongOutputDto.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingWeb.Modules.Phongs.Dto
+{
+    public class PhongOutputDto
+    {
+        public int Id { get; set; }
+
+        public List<string> HinhThucKinhDoanh { get; set; }
+
+        public List<string> DiaDiem { get; set; }
+
+        public List<string> LoaiPhong { get; set; }
+
+        public string Mota { get; set; }
+
+        public string TrangThaiPhong { get; set; }
+
+        public string DiaChiChiTiet { get; set; }
+
+        public string TenFileAnhDaiDien { get; set; }
+
+        public List<string> DichVuTienIch { get; set; }
+
+        public List<string> ChinhSach { get; set; }
+
+        public List<string> Anh { get; set; }
+
+    }
+}
