ab4f351fbfc04f8b124758c3e61039cca8f82e04
Tien192 <tranxuantien192@gmail.com>
2023-07-06 14:56:01 +0700

    test

---
+++ aspnet-core/src/BookingWeb.Application/Modules/ChiTietDatPhongs/Dto/ChiTietDatPhongDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingWeb.Modules.ChiTietDatPhongs.Dto
+{
+    public class ChiTietDatPhongDto
+    {
+        public int Id { get; set; }
+
+        public string TrangThaiPhong { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public int SLNguoiLon { get; set; }
+
+        public int SLTreEm { get; set; }
+
+        public float TienPhongQuaHan { get; set; }
+
+        public float ChiPhiHuyPhong { get; set; }
+
+        public DateTime NgayHuy { get; set; }
+
+        public float TongTien { get; set; }
+
+        public int PhongId { get; set; }
+
+        public int DatPhongId { get; set; }
+
+
+    }
+}
