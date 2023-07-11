79520d301f188a2178768ab04ed1e59b843f8111
Tian192 <tranxuantien192@gmail.com>
2023-07-11 13:46:40 +0700

    update for BwHinhThucPhong

--- aspnet-core/src/BookingWeb.EntityFrameworkCore/Migrations/20230711064456_add_colunm_AnhDaiDien_Tb_HinhThucPhong.Designer.cs
+++ aspnet-core/src/BookingWeb.EntityFrameworkCore/Migrations/20230711064456_add_colunm_AnhDaiDien_Tb_HinhThucPhong.Designer.cs
@@ -4,6 +4,7 @@ using BookingWeb.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingWeb.Migrations
 {
     [DbContext(typeof(BookingWebDbContext))]
-    partial class BookingWebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230711064456_add_colunm_AnhDaiDien_Tb_HinhThucPhong")]
+    partial class add_colunm_AnhDaiDien_Tb_HinhThucPhong
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
