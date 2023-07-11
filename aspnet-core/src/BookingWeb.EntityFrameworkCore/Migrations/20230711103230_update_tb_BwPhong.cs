22d21b18e9965408649e283a10349337e3f6cdf5
Tian192 <tranxuantien192@gmail.com>
2023-07-11 17:33:30 +0700

    update-dtb

---
+++ aspnet-core/src/BookingWeb.EntityFrameworkCore/Migrations/20230711103230_update_tb_BwPhong.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_tb_BwPhong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MienPhiHuyPhong",
+                table: "BwPhong",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MienPhiHuyPhong",
+                table: "BwPhong");
+        }
+    }
+}
