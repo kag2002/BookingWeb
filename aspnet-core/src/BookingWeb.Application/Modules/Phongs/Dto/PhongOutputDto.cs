9f6a7d990ad63fa6128fb1ed14a1c5e99ba32d89
Tian192 <tranxuantien192@gmail.com>
2023-07-18 11:37:54 +0700

    finished get room by id and get all room

--- aspnet-core/src/BookingWeb.Application/Modules/Phongs/Dto/PhongOutputDto.cs
+++ aspnet-core/src/BookingWeb.Application/Modules/Phongs/Dto/PhongOutputDto.cs
@@ -1,11 +1,5 @@
 ﻿using BookingWeb.Modules.SearchingFilter.Dto;
-using Microsoft.AspNetCore.Http;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookingWeb.Modules.Phongs.Dto
 {
